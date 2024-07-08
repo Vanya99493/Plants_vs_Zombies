ae71130382bf0c0c2aa883f52578f50c1a5e3caf
Ваня Задорожній <vanyazadorozhnii99493@gmail.com>
2024-07-08 11:06:42 +0300

    Add levels preview images for difficulties

---
+++ Assets/Scripts/UIModule/GameMenuModule/Panels/SelectDifficultyModule/DifficultyControlPanel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIModule.MainMenuModule
+{
+    public class DifficultyControlPanel : MonoBehaviour
+    {
+        [SerializeField] private Slider _difficultySlider;
+        [SerializeField] private Image _difficultyImage;
+        [SerializeField] private Sprite[] _sprites;
+
+        public void Initialize(Action<float> OnDifficultySliderChange)
+        {
+            _difficultySlider.onValueChanged.AddListener(
+                newIndex =>
+                {
+                    this.OnDifficultySliderChange(newIndex);
+                    OnDifficultySliderChange?.Invoke(newIndex);
+                });
+            this.OnDifficultySliderChange(0);
+        }
+
+        private void OnDifficultySliderChange(float newIndex)
+        {
+            _difficultyImage.sprite = _sprites[(int)newIndex];
+        }
+    }
+}
--- Assets/Scripts/UIModule/GameMenuModule/Panels/SelectDifficultyModule/SelectDifficultyPanel.cs
+++ Assets/Scripts/UIModule/GameMenuModule/Panels/SelectDifficultyModule/SelectDifficultyPanel.cs
@@ -8,15 +8,14 @@ namespace UIModule.MainMenuModule
     {
         [SerializeField] private Button _backButton;
         [SerializeField] private Button _loadLevelButton;
-        [SerializeField] private Slider _difficultySlider;
-        [SerializeField] private Image _difficultyImage;
+        [SerializeField] private DifficultyControlPanel _difficultyControlPanel;
         
         public void Initialize(Action OnBackButtonClick, Action OnLoadLevelButtonClick, 
             Action<float> OnDifficultySliderChange)
         {
             _backButton.onClick.AddListener(() => OnBackButtonClick?.Invoke());
             _loadLevelButton.onClick.AddListener(() => OnLoadLevelButtonClick?.Invoke());
-            _difficultySlider.onValueChanged.AddListener(newIndex => OnDifficultySliderChange?.Invoke(newIndex));
+            _difficultyControlPanel.Initialize(OnDifficultySliderChange);
         }
     }
 }

f507a0ee824cdf8f95a3ee2756e85b5ad3e4a326
Ваня Задорожній <vanyazadorozhnii99493@gmail.com>
2024-07-09 10:49:37 +0300

    Add adaptable camera for screen resolutions

---
+++ Assets/Scripts/CameraModule/CameraScaler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace CameraModule
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraScaler : MonoBehaviour
+    {
+        [SerializeField] private Camera _mainCamera;
+        [SerializeField] private Transform[] _points;
+        [SerializeField] private float _increaseStep = 0.1f;
+
+        private void Awake()
+        {
+            _mainCamera ??= GetComponent<Camera>();
+        }
+
+        void Start()
+        {
+            if (_points.Length != 4)
+            {
+                Debug.LogError("It is necessary to specify exactly 4 points!");
+                return;
+            }
+
+            AdjustCameraSize();
+        }
+
+        private void AdjustCameraSize()
+        {
+            bool allPointsVisible = false;
+
+            while (!allPointsVisible)
+            {
+                allPointsVisible = true;
+
+                foreach (var point in _points)
+                {
+                    Vector3 viewportPos = _mainCamera.WorldToViewportPoint(point.position);
+                    if (viewportPos.x < 0 || viewportPos.x > 1 || viewportPos.y < 0 || viewportPos.y > 1)
+                    {
+                        allPointsVisible = false;
+                        break;
+                    }
+                }
+
+                if (!allPointsVisible)
+                {
+                    _mainCamera.orthographicSize += _increaseStep;
+                }
+            }
+        }
+    }
+}
